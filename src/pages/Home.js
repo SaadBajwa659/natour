312e23d5bf1150c12f5f552c4a3abe855fc6d207
SaadBajwa659 <saad23122002@gmail.com>
2022-09-22 22:18:50 +0500

    Almost done with the header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
 import React from "react";
+import "../styles/Base.scss";
+import "../styles/Booking.scss";
+import "../styles/Description.scss";
+import "../styles/Features.scss";
+import "../styles/Footer.scss";
+import "../styles/Header.scss";
+import "../styles/Review.scss";
+import "../styles/Tours.scss";
 
 const Home = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <header className="header">
+        <div className="header__logo-box">
+          <img src="img/logo-white.png" alt="Logo" className="header__logo" />
+        </div>
+        <div className="header__text-box">
+          <h1 className="header__heading-primary">
+            <span className="header__heading-primary__main">Outdoors</span>
+            <span className="header__heading-primary__sub">
+              is where life happens
+            </span>
+          </h1>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Home;
