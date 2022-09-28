4da538bf60cf2b6c00d63f0fc7e4ba72fd500385
SaadBajwa659 <saad23122002@gmail.com>
2022-09-28 22:07:24 +0500

    Builded a custom grid layout and then commented it for future use in this project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,7 @@ import "../sass/base/Utilities.scss";
 import "../sass/components/Button.scss";
 
 import "../sass/layout/Footer.scss";
+import "../sass/layout/Grid.scss";
 import "../sass/layout/Header.scss";
 
 import "../sass/pages/Home.scss";
@@ -34,6 +35,79 @@ const Home = () => {
           <a href="#" className="btn btn--white btn--animated">Discover our tours</a>
         </div>
       </header>
+      {/* 
+      <section className="grid-test">
+
+        <div className="row">
+          <div className="col-1-of-2">
+            Col 1 of 2
+          </div>
+          <div className="col-1-of-2">
+            Col 1 of 2
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-1-of-3">
+            Col 1 of 3
+          </div>
+          <div className="col-1-of-3">
+            Col 1 of 3
+          </div>
+          <div className="col-1-of-3">
+            Col 1 of 3
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-1-of-3">
+            Col 1 of 3
+          </div>
+          <div className="col-2-of-3">
+            Col 2 of 3
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-1-of-4">
+            Col 1 of 4
+          </div>
+          <div className="col-1-of-4">
+            Col 1 of 4
+          </div>
+          <div className="col-1-of-4">
+            Col 1 of 4
+          </div>
+          <div className="col-1-of-4">
+            Col 1 of 4
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-1-of-4">
+            Col 1 of 4
+          </div>
+          <div className="col-1-of-4">
+            Col 1 of 4
+          </div>
+          <div className="col-2-of-4">
+            Col 1 of 4
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-1-of-4">
+            Col 1 of 4
+          </div>
+          <div className="col-3-of-4">
+            Col 3 of 4
+          </div>
+        </div>
+      </section>
+       */}
+
+
+
     </div>
   );
 };
