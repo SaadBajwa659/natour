c3bf2b0b0d5207c4d5e9e3e279c27ced4eaa7ad2
SaadBajwa659 <saad23122002@gmail.com>
2022-10-01 22:12:51 +0500

    added the comments and all that in about section and i am almost done with it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,19 +38,52 @@ const Home = () => {
 
       <main>
         <section className="section-about">
-          <div className="u-center-text">
+          <div className="u-center-text u-margin-bottom-big">
             <h2 className="heading-secondary">
               Exciting tours for adventurous people
             </h2>
           </div>
           <div className="row">
             <div className="col-1-of-2">
-              Text content
+              <h3 className="heading-tertiary u-margin-bottom-small">
+                You're going to fall in love with nature
+              </h3>
+              <p className="paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, ipsum sapiente aspernatur libero repellat quis
+                consequatur ducimus quam nisi exercitationem omnis earum qui.
+              </p>
+              <h3 className="heading-tertiary u-margin-bottom-small">
+                Live adventures like you never have before
+              </h3>
+              <p className="paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores nulla deserunt voluptatum nam.
+              </p>
+              <a href="#" className="btn-text">
+                Learn more &rarr;
+              </a>
             </div>
             <div className="col-1-of-2">
-              Image composition
+              <div className="composition">
+                <img
+                  src="img/nat-1.jpg"
+                  alt="Photo 1"
+                  className="composition__photo composition__photo--p1"
+                />
+                <img
+                  src="img/nat-2.jpg"
+                  alt="Photo 2"
+                  className="composition__photo composition__photo--p2"
+                />
+                <img
+                  src="img/nat-3.jpg"
+                  alt="Photo 3"
+                  className="composition__photo composition__photo--p3"
+                />
+              </div>
             </div>
-        </div>
+          </div>
         </section>
       </main>
 
