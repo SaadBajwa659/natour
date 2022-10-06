ca972f82755140ffce7d3c0113ac0a17fe333a19
SaadBajwa659 <saad23122002@gmail.com>
2022-10-06 22:01:17 +0500

    Done with the navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,12 +22,34 @@ import "../sass/components/Story.scss";
 import "../sass/layout/Footer.scss";
 import "../sass/layout/Grid.scss";
 import "../sass/layout/Header.scss";
+import "../sass/layout/Navigation.scss";
 
 import "../sass/pages/Home.scss";
 
 const Home = () => {
   return (
     <div>
+
+      <div className="navigation">
+        <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+
+        <label htmlFor="navi-toggle" className="navigation__button">
+          <span className="navigation__icon">&nbsp;</span>
+        </label>
+
+        <div className="navigation__background">&nbsp;</div>
+
+        <nav className="navigation__nav">
+          <ul className="navigation__list">
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>01</span>About Natours</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>02</span>Your Benefits</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>03</span>Popular tours</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>04</span>Stories</a></li>
+            <li className="navigation__item"><a href="#" className="navigation__link"><span>05</span>Book Now</a></li>
+          </ul>
+        </nav>
+      </div>
+
       <header className="header">
         <div className="header__logo-box">
           <img src="img/logo-white.png" alt="Logo" className="header__logo" />
