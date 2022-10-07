c419de8dc8dbc91b6c377ee3d9746ab4b433760d
SaadBajwa659 <saad23122002@gmail.com>
2022-10-07 22:14:21 +0500

    half way there with pure scss popup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,7 @@ import "../sass/components/Card.scss";
 import "../sass/components/Composition.scss";
 import "../sass/components/Feature-box.scss";
 import "../sass/components/Form.scss";
+import "../sass/components/Popup.scss";
 import "../sass/components/Story.scss";
 
 import "../sass/layout/Footer.scss";
@@ -29,9 +30,12 @@ import "../sass/pages/Home.scss";
 const Home = () => {
   return (
     <div>
-
       <div className="navigation">
-        <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+        <input
+          type="checkbox"
+          className="navigation__checkbox"
+          id="navi-toggle"
+        />
 
         <label htmlFor="navi-toggle" className="navigation__button">
           <span className="navigation__icon">&nbsp;</span>
@@ -41,11 +45,31 @@ const Home = () => {
 
         <nav className="navigation__nav">
           <ul className="navigation__list">
-            <li className="navigation__item"><a href="#" className="navigation__link"><span>01</span>About Natours</a></li>
-            <li className="navigation__item"><a href="#" className="navigation__link"><span>02</span>Your Benefits</a></li>
-            <li className="navigation__item"><a href="#" className="navigation__link"><span>03</span>Popular tours</a></li>
-            <li className="navigation__item"><a href="#" className="navigation__link"><span>04</span>Stories</a></li>
-            <li className="navigation__item"><a href="#" className="navigation__link"><span>05</span>Book Now</a></li>
+            <li className="navigation__item">
+              <a href="#" className="navigation__link">
+                <span>01</span>About Natours
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="#" className="navigation__link">
+                <span>02</span>Your Benefits
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="#" className="navigation__link">
+                <span>03</span>Popular tours
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="#" className="navigation__link">
+                <span>04</span>Stories
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a href="#" className="navigation__link">
+                <span>05</span>Book Now
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
@@ -474,14 +498,46 @@ const Home = () => {
               <a href="#" className="footer__link">
                 Advanced CSS and Sass
               </a>{" "}
-              . Copyright &copy; by Jonas Schmedtmann. You are 100% allowed to use
-              this webpage for both personal and commercial use, but NOT to
+              . Copyright &copy; by Jonas Schmedtmann. You are 100% allowed to
+              use this webpage for both personal and commercial use, but NOT to
               claim it as your own design. A credit to the original author,
               Jonas Schmedtmann, is of course highly appreciated!
             </p>
           </div>
         </div>
       </footer>
+
+      <div className="popup">
+        <div className="popup__content">
+          <div className="popup__left">
+            <img src="img/nat-8.jpg" alt="Tour photo" className="popup__img" />
+            <img src="img/nat-9.jpg" alt="Tour photo" className="popup__img" />
+          </div>
+          <div className="popup__right">
+            <h2 className="heading-secondary u-margin-bottom-small">Start booking now</h2>
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              Important &ndash; Please read these terms before booking
+            </h3>
+            <p className="popup__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed
+              sed risus pretium quam. Aliquam sem et tortor consequat id.
+              Volutpat odio facilisis mauris sit amet massa vitae. Mi bibendum
+              neque egestas congue. Placerat orci nulla pellentesque dignissim
+              enim sit. Vitae semper quis lectus nulla at volutpat diam ut
+              venenatis. Malesuada pellentesque elit eget gravida cum sociis
+              natoque penatibus et. Proin fermentum leo vel orci porta non
+              pulvinar neque laoreet. Gravida neque convallis a cras semper.
+              Molestie at elementum eu facilisis sed odio morbi quis. Faucibus
+              vitae aliquet nec ullamcorper sit amet risus nullam eget. Nam
+              libero justo laoreet sit. Amet massa vitae tortor condimentum
+              lacinia quis vel eros donec. Sit amet facilisis magna etiam.
+              Imperdiet sed euismod nisi porta.
+            </p>
+            <a href="#" className="btn btn--green">Book now</a>
+          </div>
+        </div>
+      </div>
 
       {/* 
       <section className="grid-test">
